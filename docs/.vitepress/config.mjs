cf98d474d439c564a0bd21699cfe4aa115ee00da
Uriel <devuri@users.noreply.github.com>
2024-05-24 02:51:29 -0500

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -48,6 +48,7 @@ export default defineConfig({
                     { text: 'Migration', link: '/guide/migration' },
                     { text: 'Deploy', link: '/guide/deploy' },
                     { text: 'Repo Strategy', link: '/guide/repo-strategy' },
+		    { text: 'Automated rsync Strategy', link: '/guide/rsync-strategy' },
                     { text: 'SSH Keys', link: '/guide/ssh-keys' }
                 ]
             },
