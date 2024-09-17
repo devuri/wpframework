d2cfef88d5bdceb685e23167bacd7ed639278d00
devuri <devuri@users.noreply.github.com>
2024-09-17 12:55:18 -0500

    fix: update stubs

--- src/inc/stubs/bootstrap.php
+++ src/inc/stubs/bootstrap.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the WPframework package.
- *
- * (c) Uriel Wilson <uriel@wpframework.io>
- *
- * The full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use WPframework\App;
 
 /*
